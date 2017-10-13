608061bfa3e4f38120eb400b26e33fc39faf3f41
clack <clackken.smith@gmail.com>
2017-10-13 10:44:58 +0800

    commit admin

---
+++ application/controllers/Admin.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by clack.
+ * Filename: Admin.php
+ * Date: 10/13/17
+ * Time: 10:39 AM
+ */
+/*========================================*/
+
+class Admin extends CI_Controller{
+    function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function index(){
+        $this->load->view('Admin/index');
+    }
+}
---
+++ application/views/Admin/index.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by clack.
+ * Filename: index.php
+ * Date: 10/13/17
+ * Time: 10:42 AM
+ */
+/*========================================*/
